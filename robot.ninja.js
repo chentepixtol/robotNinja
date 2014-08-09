3c6a0f6b36f2c0f7bd78ea2ec2622d3d6b8c3b7e
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2014-08-08 23:29:50 -0500

    added Event class

--- robot.ninja.js
+++ robot.ninja.js
@@ -58,69 +58,102 @@ var Robot = function(robot) {
 
   function composite(functions){
     return function() {
-      var context = this;
       var args = Array.prototype.slice.call(arguments, 0);
       var response = [];
       functions.forEach(function(fn) {
-        response.push(fn.apply(context, args));
+        response.push(fn.apply(fn, args));
       });
       return response;
     };
   }
+
+  var Event = {
+    events: {},
+    on: function(eventName, callback) {
+      if (this.events[eventName]) {
+        this.events[eventName].push(callback);
+      } else {
+        this.events[eventName] = [callback];
+      }
+    },
+    trigger: function(eventName, options) {
+      if (this.events[eventName]) {
+        return composite(this.events[eventName]).call(this, options);
+      }
+      return [];
+    }
+  };
 
   var Strategy  = {
     events: {},
     get: function(name) {
       return this.events[name] ? composite(this.events[name]) : function(){};
     },
-    add: function(name, callback) {
-      if (this.events[name]) {
-        this.events[name].push(callback);
-      } else {
-        this.events[name] = [callback];
+    add: function(name, callback, context) {
+      if (/^on[a-z]+/i.test(name) && typeof callback === 'function') {
+        if (this.events[name]) {
+          this.events[name].push(callback.bind(context));
+        } else {
+          this.events[name] = [callback.bind(context)];
+        }
       }
     },
     append: function(object) {
       for(var property in object){
-        this.add(property, object[property]);
+        this.add(property, object[property], object);
       }
     },
     replace: function(object) {
       for(var property in object){
         delete this.events[property];
-        this.add(property, object[property]);
+        this.add(property, object[property], object);
       }
     },
   };
 
   var MoveToCenterStrategy = {
-    onIdle: function() {
-      Arena.moveToCenter(this);
+    onIdle: function(ev) {
+      Arena.moveToCenter(ev.robot);
     }
   };
 
+  var SeekerStrategy = {
+    message: "hola robot",
+    onIdle: function(ev) {
+      var robot = ev.robot;
+      robot.turn(25);
+      robot.fire();
+      robot.log(this.message);
+    }
+  };
+  Strategy.append(SeekerStrategy);
+
   Robot.prototype.onIdle = function(ev) {
-    Strategy.get('onIdle').call(ev.robot, ev);
+    Strategy.get('onIdle').call(null, ev);
   };
 
   Robot.prototype.onScannedRobot = function(ev) {
-    Strategy.get('onScannedRobot').call(ev.robot, ev);
+    Strategy.get('onScannedRobot').call(null, ev);
   };
 
   Robot.prototype.onRobotCollision = function(ev) {
-    Strategy.get('onRobotCollision').call(ev.robot, ev);
+    Strategy.get('onRobotCollision').call(null, ev);
   };
 
   Robot.prototype.onWallCollision = function(ev) {
-    Strategy.get('onWallCollision').call(ev.robot, ev);
+    Strategy.get('onWallCollision').call(null, ev);
   };
 
   Robot.prototype.onHitByBullet = function(ev) {
-    Strategy.get('onHitByBullet').call(ev.robot, ev);
+    Strategy.get('onHitByBullet').call(null, ev);
   };
 
-  Robot.prototype.getStrategy = function(ev) {
+  Robot.prototype.getStrategy = function() {
     return Strategy;
+  };
+
+  Robot.prototype.getEvent = function() {
+    return Event;
   };
 
 })();
